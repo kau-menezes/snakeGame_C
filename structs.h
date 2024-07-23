c4b90e14c41351ba5c324c2576f96a6c5541e757
akauzinha47 <kau.vmenezes@gmail.com>
2024-07-23 16:28:34 -0300

    feat: snake and apple constructors

--- structs.h
+++ structs.h
@@ -3,18 +3,40 @@
 
 typedef struct {
 
-    int current_position[2] = {20, 20};
+    int current_position[2];
     int next_position[2];
-    int direction = 0;
-    int length = 1;
-    int value = 1;
+    int direction;
+    int length;
+    int value;
 
 } Snake;
+
+Snake construct_snake()
+{
+    Snake new_snake;
+    new_snake.current_position[0] = 20;
+    new_snake.current_position[1] = 20;
+    new_snake.direction = 3;
+    new_snake.length = 1;
+    new_snake.value = 1;
+
+    return new_snake;
+
+}
 
 typedef struct {
     
     int current_position[2];
-    int value = 9;
+    int value;
 
 } Apple;
+
+Apple construct_apple()
+{
+    Apple new_apple;
+    new_apple.value = 9;
+
+    return new_apple;
+}
+
 #endif // !STRUCTS
