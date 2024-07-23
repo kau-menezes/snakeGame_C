b47c57eab8e8f0598b34dd40341fa6e3c5b5d816
akauzinha47 <kau.vmenezes@gmail.com>
2024-07-23 14:24:56 -0300

    feat: snake struct

--- structs.h
+++ structs.h
@@ -1,6 +1,12 @@
 #ifndef STRUCTS
 #define STRUCTS
 
+typedef struct {
 
+    int current_position[2] = {20, 20};
+    int next_position[2];
+    int direction = 0;
+    int length = 1;
 
+} Snake;
 #endif // !STRUCTS
