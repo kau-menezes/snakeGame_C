4ab3a7faafe4b6bba16346fa2f57da2d9adea55e
akauzinha47 <kau.vmenezes@gmail.com>
2024-07-23 14:55:22 -0300

    feat: map rendering and apple generating location

--- render_map.h
+++ render_map.h
@@ -1,4 +1,59 @@
 #ifndef RENDER_MAP
 #define RENDER_MAP
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "structs.h"
+
+#define MAP_HEIGHT 40
+#define MAP_WIDTH 40
+
+void generate_apple(int map[MAP_HEIGHT][MAP_WIDTH], Apple *apple)
+{
+    srand(time(NULL)); 
+
+    apple->current_position[0]= rand() % 40;
+    apple->current_position[1] = rand() % 40;
+
+    map[apple->current_position[1]][apple->current_position[0]] = apple->value;
+
+}
+
+void render_map(int map[MAP_HEIGHT][MAP_WIDTH])
+{
+    for (int i = 0; i < MAP_WIDTH; i++) 
+    {
+        printf("#");
+    }
+
+    for ( int i = 0; i < MAP_HEIGHT; i++)
+    {
+        printf("#");
+
+        for (int j = 0; j < MAP_WIDTH; j++)
+        {
+            if (map[i][j] == 0)
+            {
+                printf(" ");
+            } else if (map[i][j] == 1) {
+                printf("o");
+            } else if (map[i][j] == 9) {
+                printf("+");
+            }
+        }    
+        
+        printf("#");
+        printf("\n");
+       
+    }
+    
+    for (int i = 0; i < MAP_WIDTH; i++) 
+    {
+        printf("#");
+    }
+
+}
+
 #endif // !RENDER_MAP
--- structs.h
+++ structs.h
@@ -7,6 +7,14 @@ typedef struct {
     int next_position[2];
     int direction = 0;
     int length = 1;
+    int value = 1;
 
 } Snake;
+
+typedef struct {
+    
+    int current_position[2];
+    int value = 9;
+
+} Apple;
 #endif // !STRUCTS
