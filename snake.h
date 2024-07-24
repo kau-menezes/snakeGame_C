1bc0e26ec7ad6a2165b671c6464415866900d86a
akauzinha47 <kau.vmenezes@gmail.com>
2024-07-24 11:50:43 -0300

    refact: snake movement

--- snake.h
+++ snake.h
@@ -6,33 +6,69 @@
 #include <time.h>
 
 #include "structs.h" 
+#include "render_map.h" 
 
-void snake_movement(Snake *snake)
+void snake_next_movement(Snake *snake)
 {
-    switch (snake->direction)
+
+    // dictates the snake's head next position by its direction
+    switch (snake->head->direction)
     {
+        case 0:
+            snake->head->next_position[1] = snake->head->current_position[1] + 1;
+            break;
 
-    case 0:
-        snake->next_position[1] = snake->current_position[1] + 1;
-        break;
+        case 3:
+            snake->head->next_position[0] = snake->head->current_position[0] + 1;
+            break;
 
-    case 3:
-        snake->next_position[0] = snake->current_position[0] + 1;
-        break;
+        case 6:
+            snake->head->next_position[1] = snake->head->current_position[1] - 1;
+            break;
 
-    case 6:
-        snake->next_position[1] = snake->current_position[1] - 1;
-        break;
-
-    case 9:
-        snake->next_position[0] = snake->current_position[0] - 1;
-        break;
-    
-    default:
-        break;
+        case 9:
+            snake->head->next_position[0] = snake->head->current_position[0] - 1;
+            break;
+        
+        default:
+            break;
     }
 
+    SnakeNode * current = snake->head->next;
+
+    while (current->next != NULL)
+    {
+        current->next_position[0] = current->next->current_position[0]; 
+        current->next_position[1] = current->next->current_position[1]; 
+
+        current = current->next;
+    }
     
+}
+
+void snake_movement(int map[MAP_HEIGHT][MAP_WIDTH], Snake *snake)
+{
+    SnakeNode current = snake->head;
+
+    while (current->next != NULL)
+    {
+        // remove its previous space in the map
+        map[current->current_position[1]][current->current_position[0]] = 0
+        
+        // appears in its previously thought next position
+        map[current->next_position[1]][current->next_position[0]] = snake->value
+        
+        // itters trough the loop
+        current = current->next;
+    }
+}
+
+void verify_snake_snack(Snake *snake, Apple *apple)
+{
+    if (snake->head->current_position[0] == apple.current_position[0] && snake->head->current_position[0] == apple.current_position[0])
+    {
+        points++;
+    }
 }
 
 #endif // !SNAKE
--- structs.h
+++ structs.h
@@ -3,31 +3,38 @@
 
 typedef struct {
 
+    int current_position[2];
+    int next_position[2];
+    SnakeNode next;
+
+} SnakeNode;
+
+typedef struct {
+
     int direction;
     int length;
     int value;
-    SnakeNode head;
+    SnakeNode* head;
 
 } Snake;
 
 Snake construct_snake()
 {
     Snake new_snake;
+    
     new_snake.direction = 3;
     new_snake.length = 1;
     new_snake.value = 1;
 
+    SnakeNode new_node;
+    new_node.current_position[0] = 20;
+    new_node.current_position[1] = 20;
+
+    new_node.next_position[0] = 20;
+    new_node.next_position[1] = 21;
+
     return new_snake;
-
 }
-
-typedef struct {
-
-    int current_position[2];
-    int next_position[2];
-    SnakeNode next;
-
-} SnakeNode;
 
 typedef struct {
     
